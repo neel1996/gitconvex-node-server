3e9856dda2ce41558e7d02b29bf06349f52a1626
neel1996 <neelusenthil1996@gmail.com>
2020-04-06 22:10:16 +0530

    Add repo API and JSON datastore included

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const env = require("dotenv").config();
-const healtchehck = require('./healthcheck');
-const fetchRepo = require('./repofetchapi')
+const healtchehck = require("./healthcheck");
+const fetchRepo = require("./repofetchapi");
+const storeRepo = require("./addRepo");
 
 healtchehck.listen(process.env.PORT_HEALTHCHECK || 5000, err => {
   if (err) {
     console.log("HealthCheck API error occurred : " + err);
   }
-  console.log("API HealthCheck started @ PORT : " +process.env.PORT_HEALTHCHECK);
+  console.log(
+    "API HealthCheck started @ PORT : " + process.env.PORT_HEALTHCHECK
+  );
 });
 
-fetchRepo.listen(process.env.PORT_FETCHREPO || 5001, err=>{
-  if(err){
-    console.log("FetRepo API error occurred : " +err)
+fetchRepo.listen(process.env.PORT_FETCHREPO || 5001, err => {
+  if (err) {
+    console.log("FetRepo API error occurred : " + err);
   }
-  console.log("API FetchRepo started @ PORT : " +process.env.PORT_FETCHREPO);
-})
+  console.log("API FetchRepo started @ PORT : " + process.env.PORT_FETCHREPO);
+});
+
+storeRepo.listen(process.env.PORT_ADDREPO || 5002, err => {
+  if (err) {
+    console.log("AddRepo API error occurred : " + err);
+  }
+  console.log("API AddRepo started @ PORT : " + process.env.PORT_ADDREPO);
+});
